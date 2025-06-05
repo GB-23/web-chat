88ef47df19407da52ffcf360cdc3bb2543400d22
BlenTh <jgui.alves1012@gmail.com>
2025-06-05 09:53:10 -0300

    novo
    saf

--- client/class.js
+++ client/class.js
@@ -1,9 +1,18 @@
-class User {
-  constructor(name, password, token, dateTime, encryptopass) {
-    this.name = name;
-    this.password = password;
-    this.token = token;
-    this.dateTime = dateTime;
-    this.encryptopass = encryptopass;
-  }
+let user1 = 
+{
+  id: "0",
+  level : "User", 
+  name : "Gabriel",
+  mansage,
+  Image: "./public/page/image/190952628.jfif",
+  token : "",
+}
+let user2 = 
+{
+  id: "1",
+  level : "User", 
+  name : "Joao",
+  mansage,
+  Image: "./public/page/image/190793681.jfif",
+  token : "",
 }
